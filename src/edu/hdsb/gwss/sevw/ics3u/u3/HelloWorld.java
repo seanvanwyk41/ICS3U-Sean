6b8faaaa68c9c96d2b4b8045e93e7f453f9b4048
Sean <Sean@10.96.177.240>
2015-02-24 10:56:37 -0500

    -initialize Repository First Time-

---
+++ src/edu/hdsb/gwss/sevw/ics3u/u3/HelloWorld.java
@@ -0,0 +1,33 @@
+/*
+ * Name: HellowWorld.java
+ * Date: 24-02-2015 (DD-MM-YYYY)
+ * Version: v0.01
+ * Author: Sean van Wyk
+ * Description: Simple Program To Display Hello World to the screan
+*/
+package edu.hdsb.gwss.sevw.ics3u.u3;
+
+/**
+ *
+ * @author Sean van Wyk
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+        if (1+1==2) {
+            System.out.println("Correct");
+            
+        }
+       
+            
+        
+       
+       
+    }
+    
+}
+
