a55f1ec6dd6d640286e4ecacbb3c016ee49a2a60
Sean <Sean@10.96.177.240>
2015-02-24 10:59:36 -0500

    -Updated Hello World to Remove all emotion simulation syntax-

--- src/edu/hdsb/gwss/sevw/ics3u/u3/HelloWorld.java
+++ src/edu/hdsb/gwss/sevw/ics3u/u3/HelloWorld.java
@@ -1,7 +1,7 @@
 /*
  * Name: HellowWorld.java
  * Date: 24-02-2015 (DD-MM-YYYY)
- * Version: v0.01
+ * Version: v0.02
  * Author: Sean van Wyk
  * Description: Simple Program To Display Hello World to the screan
 */
@@ -17,7 +17,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld");
         if (1+1==2) {
             System.out.println("Correct");
             
