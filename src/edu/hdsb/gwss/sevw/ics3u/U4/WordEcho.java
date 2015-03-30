5740e87c068e5ce54acedd4aefcbc6f81284db31
Sean <Sean@199.235.249.9>
2015-03-30 11:01:58 -0400

    Started work on WordEcho.java

---
+++ src/edu/hdsb/gwss/sevw/ics3u/U4/WordEcho.java
@@ -0,0 +1,38 @@
+/*
+* Name: WordEcho
+* Date: 30-03-2015 (DD-MM-YYYY)
+* Version: v0.1
+* Author: Sean van Wyk
+ * Description: Program to repeat a word the same amount of times it has letters
+ */
+package edu.hdsb.gwss.sevw.ics3u.U4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sean
+ */
+public class WordEcho {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //Variables
+    int length;
+    
+    //Objects
+    Scanner input = new Scanner(System.in);
+    
+    //ask get
+    System.out.println("Enter word to pass");
+    String word = input.nextLine();
+    
+    //Calculate variable value
+    int length = .length( word );
+    
+    
+    }
+    
+}
